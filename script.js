dc7a033cc26aab76cafdcb9b389e75ba31965a23
derik10k <deriksam10k@gmail.com>
2025-03-21 01:30:32 -0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// Simple JavaScript for project interaction (just an example)
-
-document.querySelectorAll('.project').forEach(project => {
-    project.addEventListener('click', function() {
-        alert('You clicked on ' + this.querySelector('h3').innerText);
-    });
-});
